b7168821bcdb440e8933d3dd91b78d68964c4c08
Alan Miranda <alannub6@gmail.com>
2024-11-21 16:11:05 -0300

    feat: inclui `lombok` no projeto para gerar Get, Set e Construtores. E adiciona ele nas classes `MissoesModel` e `NinjaModel`

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,11 +3,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data //Cria todos Get e Set
 public class MissoesModel {
 
     @Id

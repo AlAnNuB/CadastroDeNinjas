40ec4c48bef7f55939b3db299be4c5b1de9b17a5
Alan Miranda <alannub6@gmail.com>
2024-11-26 14:46:39 -0300

    feat: Cria as rotas Controller do `MissoesController` com mock

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,29 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import jakarta.persistence.Entity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missão listada com Sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão Criada com Sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão Alterada com Sucesso";
+    }
+
+    @DeleteMapping("/alterar")
+    public String deletarMissao() {
+        return "Missão deletada com Sucesso";
+    }
+}
